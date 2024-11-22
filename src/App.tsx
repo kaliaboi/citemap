a003e38eca75ef91d528e824b2e8f0b311aae09f
Abhishek Kalia <abhishekkalia.productdesign@gmail.com>
2024-11-22 16:50:17 -0500

    Add shadcn components + sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import "./App.css";
+import { AppSidebar } from "./components/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <SidebarProvider>
+        <AppSidebar />
+        <main>
+          <SidebarTrigger />
+        </main>
+      </SidebarProvider>
     </>
   );
 }
